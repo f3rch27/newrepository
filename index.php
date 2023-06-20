df993a8a61572790c9481e5c25a47ae7c174dedb
f3rch27 <131517357+f3rch27@users.noreply.github.com>
2023-06-20 05:55:54 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+include('doctype.php');
+include('header.php');
+?>
+<body>
+<section class="accueil">
+    <img class="image"src="image/fond_accueil.jpg" name="fond_accueil.jpg">
+</section>    
+
+  
+  <?php
+include("footer.php");
+	?>
+
+    
+
+
+
